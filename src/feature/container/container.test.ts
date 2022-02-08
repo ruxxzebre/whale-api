6377a1a54ec08c43975f697b52b719b74a4e7e03
ruxxzebre <paullaboratory@gmail.com>
2022-02-08 12:02:59 +0200

    Init tests

---
+++ src/feature/container/container.test.ts
@@ -0,0 +1,12 @@
+// import Docker from 'dockerode';
+
+describe('Container Suite', () => {
+  it('Should pass', () => {
+    expect(0).toBeFalsy();
+  });
+  // describe('Container Controller', () => {});
+  // describe('Container Model', () => {});
+  // describe('Container Router', () => {});
+  // describe('Container Service', () => {});
+  // describe('Container Utilities', () => {});
+});
